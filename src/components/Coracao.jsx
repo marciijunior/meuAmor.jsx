0b7fae903cc0d6ff8e67fb1c26f7614c5dac95b2
marciijunior <marciojsjunior.mj@gmail.com>
2025-06-12 13:16:37 -0300

    V 1.0.0

--- src/components/Coracao.jsx
+++
@@ -1,7 +0,0 @@
-export default function Coracao() {
-  return (
-    <div className="coracao">
-        <img className="coracao" src="./assets/coracao.png" alt="Desenho"/>
-    </div>
-  );
-}
--- src/components/Desenho.jsx
+++ src/components/Desenho.jsx
@@ -1,6 +1,7 @@
 export default function Desenho() {
   return (
     <div className="desenho">
+        <img className="desenho__doguinhos" src="./assets/cachorrinho.png" alt="Doguinhos"/>
         <img className="desenho__imagem" src="./assets/desenho.png" alt="Desenho"/>
     </div>
   );
